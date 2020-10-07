106c5c0ea98f31bded5854a06175443d0f3481cb
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-10-07 10:42:46 +0200

    feat(Algorithm): add find max

---
+++ src/utils.rs
@@ -0,0 +1,33 @@
+pub fn swap(arr: &mut [usize], i: usize, j: usize) {
+    let tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+}
+
+
+pub fn max(arr: &[usize]) -> usize {
+    let mut max = std::usize::MIN;
+    for elem in arr {
+        max = max.max(*elem)
+    }
+    return max;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::utils::{swap, max};
+
+    #[test]
+    fn should_swap() {
+        let mut xs = [4, 3, 1, 6, 5];
+        swap(&mut xs, 0, 1);
+        assert_eq!(3, xs[0]);
+        assert_eq!(4, xs[1]);
+    }
+
+    #[test]
+    fn should_find_max() {
+        let xs = [1, 2, 0, 9, 5];
+        assert_eq!(9, max(&xs));
+    }
+}
